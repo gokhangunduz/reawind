62da034789aeb86e98deba9debd951824dd1d72b
gokhangunduz <mail@gokhangunduz.com.tr>
2023-02-15 00:22:11 +0300

    feat(LoadingBar): :tada: Add loadingbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AppRoutes from "./routes/AppRoutes";
 import ThemeContext from "./context/ThemeContext";
-import Sidebar from "./components/Sidebar/Sidebar";
 import SidebarContext from "./context/SidebarContext";
 import "animate.css";
+import LoadingBar from "react-top-loading-bar";
 
 function App() {
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    setProgress(100);
+  }, []);
+
   return (
     <>
       <SidebarContext>
         <ThemeContext>
+          <LoadingBar
+            height={4}
+            color={`#00ffe2`}
+            progress={progress}
+            shadow={true}
+          />
           <AppRoutes />
         </ThemeContext>
       </SidebarContext>
